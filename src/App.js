b56dcff593c9fb9d65db202c0239ffdb29c9dbd9
MawatariTanaka <buinhatminh2005@gmail.com>
2022-08-09 21:30:58 +0700

    reformatting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Login from './components/auth/Login'
-import Signup from './components/auth/SignUp'
-import { AuthProvider } from "./contexts/AuthContext";
-import PrivateRoute from "./components/auth/PrivateRoute";
-import Profile from "./components/auth/Profile"
+import Login from './components/auth/Login';
+import Signup from './components/auth/SignUp';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/auth/PrivateRoute';
+import Profile from './components/auth/Profile';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <AuthProvider>
-        <Routes>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route
-                exact
-                path="/profile"
-                element={
-                  <PrivateRoute>
-                    <Profile />
-                  </PrivateRoute>
-                }
-              ></Route>
-        </Routes>
+          <Routes>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route
+              exact
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            ></Route>
+          </Routes>
         </AuthProvider>
       </BrowserRouter>
     </>
