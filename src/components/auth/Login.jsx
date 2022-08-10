ef0845de23e053eaf1a074fa496ea2941a4603c6
MawatariTanaka <buinhatminh2005@gmail.com>
2022-08-10 09:47:23 +0700

    Add design folder

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,38 +1,38 @@
-import React, { useRef, useState } from "react";
-import { useAuth } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
-  const [error, setError] = useState("")
+  const [error, setError] = useState('');
   const { login } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        await login(emailRef.current.value, passwordRef.current.value)
-        console.log("Logged in!")
-        setError("")
-        navigate("/studio")
+      await login(emailRef.current.value, passwordRef.current.value);
+      console.log('Logged in!');
+      setError('');
+      navigate('/studio');
     } catch (error) {
-        console.log(error)
-        setError("Failed to login")
+      console.log(error);
+      setError('Failed to login');
     }
-  }
+  };
 
   return (
     <div>
       Login
       <form onSubmit={handleSubmit}>
-        {error !== "" && <p>{error}</p>}
+        {error !== '' && <p>{error}</p>}
         <div>
-            <label>Email: </label>
+          <label>Email: </label>
           <input type="email" ref={emailRef} required></input>
         </div>
         <div>
-            <label>Password: </label>
+          <label>Password: </label>
           <input type="password" ref={passwordRef} required></input>
         </div>
         <button type="submit">Login</button>
--- src/components/auth/PrivateRoute.jsx
+++ src/components/auth/PrivateRoute.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
 
-  return (
-    <div>{user ? children : <Navigate to="/login" />}</div>
-  );
+  return <div>{user ? children : <Navigate to="/login" />}</div>;
 };
 
-export default PrivateRoute;
+export default PrivateRoutes;
